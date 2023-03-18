0826c45189db619019fc8feaec073013380f4579
chanhihi <chanhihi55@gmail.com>
2023-03-18 15:47:51 +0900

    init: menubar

---
+++ Box42/Menubar/MenubarModel.swift
@@ -0,0 +1,19 @@
+//
+//  MenubarModel.swift
+//  Box42
+//
+//  Created by Chan on 2023/03/16.
+//
+
+import AppKit
+
+public class StatusBar {
+	var statusItem = NSStatusItem()
+	var frames = [NSImage]()
+	var cnt: Int = 0
+	var isRunning: Bool = false
+	var interval: Double = 1.0
+	var alertCount: Int = 0
+}
+
+
