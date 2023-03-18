1ae2bfc5eb9d78a8d94c2dfe696e6810947cfc35
chanhihi <chanhihi55@gmail.com>
2023-03-18 15:49:20 +0900

    init: web

---
+++ Box42/Web/WebViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebViewController.swift
+//  Box42
+//
+//  Created by Chan on 2023/03/16.
+//
+
+import Cocoa
+
+class WebViewController: NSViewController {
+	
+	@IBOutlet weak var webView: NSView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+}
