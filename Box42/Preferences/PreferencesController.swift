361fd85f7436518afdf236c1cbaaddb66e4f4c03
chanhihi <chanhihi55@gmail.com>
2023-08-11 04:20:01 +0900

    feat(Script📃): prototype을 완성하였습니다.

--- Box42/Preferences/PreferencesController.swift
+++ Box42/Preferences/PreferencesController.swift
@@ -6,16 +6,18 @@
 //
 
 import Cocoa
+import Foundation
 
 class PreferencesViewController: NSViewController {
-    
+    let menubarVC = MenubarViewController()
     private var stackView: NSStackView!
     private var rightView: NSView!
+    private var outputView: NSTextView!
     
     override func loadView() {
         self.view = NSView()
         self.stackView = NSStackView()
-        self.stackView.orientation = .horizontal
+        self.stackView.orientation = .vertical
         self.stackView.distribution = .fillEqually
         self.stackView.spacing = 20
         self.view.addSubview(stackView)
@@ -32,10 +34,39 @@ class PreferencesViewController: NSViewController {
         self.stackView.addArrangedSubview(leftView)
         self.stackView.addArrangedSubview(rightView)
         
-        let iconButton = NSButton(title: "Change Icon", target: self, action: #selector(changeIconButtonPressed))
-        let cronButton = NSButton(title: "Cron Script", target: self, action: #selector(cronButtonPressed))
+        outputView = NSTextView()
+        outputView.translatesAutoresizingMaskIntoConstraints = false
+        rightView.addSubview(outputView)
+
+        NSLayoutConstraint.activate([
+            outputView.topAnchor.constraint(equalTo: rightView.topAnchor),
+            outputView.leadingAnchor.constraint(equalTo: rightView.leadingAnchor),
+            outputView.trailingAnchor.constraint(equalTo: rightView.trailingAnchor),
+            outputView.bottomAnchor.constraint(equalTo: rightView.bottomAnchor)
+        ])
+
+        
+        var stackBox: [NSView] = []
+        
+        let icons = iconModel().icon
+        icons.forEach { (icon) in
+            stackBox.append(NSButton(title: "Change \(icon) Icon", target: self, action: #selector(changeIconButtonPressed)))
+        }
+        
+        let scripts = Scripts().info
+        scripts.forEach { (script) in
+            stackBox.append(NSButton(title: "\(script.name) Script: \(script.description)", target: self, action: #selector(scriptButtonPressed)))
+            
+        }
+        
+//        let scriptButton = NSButton(title: "Script", target: self, action: #selector(scriptButtonPressed))
+        let appleScriptButton = NSButton(title: "Apple Script", target: self, action: #selector(scriptButtonPressed))
         let etcButton = NSButton(title: "Etc.", target: self, action: #selector(etcButtonPressed))
-        let buttonStackView = NSStackView(views: [iconButton, cronButton, etcButton])
+        
+//        stackBox.append(scriptButton)
+        stackBox.append(appleScriptButton)
+        stackBox.append(etcButton)
+        let buttonStackView = NSStackView(views: stackBox)
         buttonStackView.orientation = .vertical
         buttonStackView.distribution = .fillEqually
         buttonStackView.spacing = 20
@@ -49,12 +80,69 @@ class PreferencesViewController: NSViewController {
         ])
     }
     
-    @objc func changeIconButtonPressed() {
+    @objc func changeIconButtonPressed(_ sender: NSButton) {
         // Change the content of the right view for icon changing
+        let icon = sender.title.split(separator: " ").map{String($0)}
+        print(icon[1])
+        menubarVC.menubarStopRunning()
+        menubarVC.buttonImageChange(icon[1])
+        menubarVC.menubarStartRunning()
     }
     
-    @objc func cronButtonPressed() {
-        // Change the content of the right view for cron script selection
+    @objc func scriptButtonPressed(_ sender: NSButton) {
+        let script = sender.title.split(separator: " ").map{String($0)}
+        if script[1] == "Script:" {
+            if let scriptPath = Bundle.main.path(forResource: script[0], ofType: "sh") {
+                let task = Process()
+                task.launchPath = "/bin/sh"
+                task.arguments = [scriptPath]
+
+                let outputPipe = Pipe()
+                task.standardOutput = outputPipe
+                task.standardError = outputPipe
+                
+                
+                task.standardError = outputPipe
+                
+                outputPipe.fileHandleForReading.readabilityHandler = { [weak self] fileHandle in
+                    if #available(OSX 10.15.4, *) {
+                        if let data = try? fileHandle.readToEnd(), let output = String(data: data, encoding: .utf8) {
+                            DispatchQueue.main.async {
+                                if let outputView = self?.outputView {
+                                    outputView.string += "\(output)"
+                                } else {
+                                    print("outputView is nil")
+                                }
+                            }
+                        }
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                }
+                        
+                        
+                task.launch()
+                task.waitUntilExit()
+
+//                let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+//                let output = String(data: outputData, encoding: .utf8) ?? ""
+//                print("Output: \(output)")
+            } else {
+                print("Script not found")
+            }
+        } else if sender.title == "Apple Script" {
+            let appleScriptCode = "display dialog \"Hello, World!\""
+
+            if let appleScript = NSAppleScript(source: appleScriptCode) {
+                var errorDict: NSDictionary? = nil
+                appleScript.executeAndReturnError(&errorDict)
+                
+                if let error = errorDict {
+                    print("Error: \(error)")
+                }
+            }
+
+        }
     }
     
     @objc func etcButtonPressed() {
