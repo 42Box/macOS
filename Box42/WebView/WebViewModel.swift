4ea7f5aa61404b4e023d29dd6484ce7d9ddcd03b
chanhihi <chanhihi55@gmail.com>
2023-08-18 02:39:45 +0900

    feat(webview): ViewModel의 CRUD기능을 구현합니다.

--- Box42/WebView/WebViewModel.swift
+++ Box42/WebView/WebViewModel.swift
@@ -5,38 +5,70 @@
 //  Created by Chan on 2023/03/19.
 //
 
+import Combine
 import WebKit
 
-// Singleton
-class WebViewList {
-	static let shared = WebViewList()
+typealias URLMapping = [String: URL]
 
-	var list: [String : WKWebView]!
-	
-	private init() {
-		list = [:]
-	}
-}
-
-// CRUD 4가지 형태의 데이터 가공 create, read, update, delete
+// WebView 관련 CRUD 4가지 형태의 데이터 가공 create, read, update, delete
 class WebViewModel: ObservableObject {
-    var webViewURL: URLModels
-    @Published var URLdict: [String: URL]
-
+    @Published var webViewURL: URLModels
+    @Published var URLdict: URLMapping
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        self.webViewURL = URLModels(info: [URLModel(name: "home", url: "https://42box.github.io/front-end/")])
-        self.URLdict = [String: URL]()
+        self.webViewURL = URLModels(info: [URLModel(name: Constants.url.InitialName, url: Constants.url.InitialPage)])
+        self.URLdict = URLMapping()
+
+        $webViewURL.sink { (WVURL) in
+            self.setUpURLdict()
+        }.store(in: &cancellables)
     }
     
     func setUpURLdict() {
-//        for urlModel in webViewURL.info {
-//            URLdict[urlModel.name] = URL(string: urlModel.url)
-//        }
         for urlModel in webViewURL.URLstring {
-            URLdict[urlModel.0] = URL(string: urlModel.1)
+            URLdict[urlModel.name] = URL(string: urlModel.url)
         }
     }
     
+    // Create
+    func createURL(_ name: String, _ url: String) {
+        let newURL = URLModel(name: name, url: url)
+        self.webViewURL.info.append(newURL)
+    }
+    
+    // Read
+    func readURLString(_ index: Int) -> URLModel {
+        return webViewURL.info[index]
+    }
+    
+    func readURL(_ index: Int) -> URL {
+        return URL(string: webViewURL.info[index].url) ?? URL(string: Constants.url.InitialPage)!
+    }
+    
+    func safeURL() -> URL {
+        return URL(string: webViewURL.info.first?.url ?? Constants.url.InitialPage)!
+    }
+    
+    func requestURL(_ url: URL) -> URLRequest {
+        return URLRequest(url: url)
+    }
+    
+    // Update
+    func updateURL(_ id: UUID, _ name: String, _ url: String) {
+        if let selectedIndex = webViewURL.info.firstIndex(where: { user in user.id == id }) {
+            webViewURL.info[selectedIndex].name = name
+            webViewURL.info[selectedIndex].url = url
+        }
+    }
+    
+    // Delete
+    func deleteURL(id: UUID) {
+        if let selectedIndex = webViewURL.info.firstIndex(where: { user in user.id == id }) {
+            cancellables.removeAll()
+            webViewURL.info.remove(at: selectedIndex)
+        }
+    }
     
 }
 
