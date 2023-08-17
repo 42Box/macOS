fa333ebeb40ca4ad34c9b1d77bca889546b735bc
chanhihi <chanhihi55@gmail.com>
2023-08-18 02:41:12 +0900

    refactor(webview): url모델을 업데이트합니다.

--- Box42/WebView/URL/URLModel.swift
+++ Box42/WebView/URL/URLModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+typealias nameUrl = (name: String, url: String)
+
 struct URLModel {
     var id: UUID
     var name: String
@@ -22,25 +24,19 @@ struct URLModel {
 struct URLModels {
     var info: [URLModel]
     
-    // Network logic
-    let URLstring: [(String, String)] = [
+    // Network logic api call 날려서 받아올 것.
+    let URLstring: [nameUrl] = [
         ("home", "https://42box.github.io/front-end/"),
         //        ("home", "http://127.0.0.1:3000/"),
         ("Box 42", "https://42box.github.io/front-end/#/box"),
         ("Intra 42", "https://intra.42.fr"),
         ("Jiphyeonjeon", "https://42library.kr"),
-        ("E-Library", "https://42seoul.dkyobobook.co.kr/main.ink"),
+        ("42STAT", "https://stat.42seoul.kr/home"),
         ("24Hane", "https://24hoursarenotenough.42seoul.kr"),
-        ("80000Coding", "https://80000coding.oopy.io"),
+        ("80kCoding", "https://80000coding.oopy.io"),
         ("where42", "https://www.where42.kr"),
         ("cabi", "https://cabi.42seoul.io/"),
         ("42gg", "https://42gg.kr/"),
-        ("textart", "https://textart.sh/")
+        ("textart", "https://textart.sh/"),
     ]
-    
-    mutating func urlSetup() {
-        URLstring.forEach { (name, url) in
-            info.append(URLModel(name: name, url: url))
-        }
-    }
 }
