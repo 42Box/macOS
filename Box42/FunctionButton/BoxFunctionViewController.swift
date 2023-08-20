68ecc98c412aa4e3c1027d0529ea9730e10c790c
chanhihi <chanhihi55@gmail.com>
2023-08-21 04:02:15 +0900

    refactor(function): button그룹 하단의 function탭의 뷰를 변경하였습니다.

---
+++ Box42/FunctionButton/BoxFunctionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FunctionButtonViewController.swift
+//  Box42
+//
+//  Created by Chanhee Kim on 8/20/23.
+//
+
+import Cocoa
+
+class BoxFunctionViewController: NSViewController {
+    override func loadView() {
+        let functionViewGroup = BoxFunctionViewGroup()
+        
+        functionViewGroup.preferenceAction = preference
+        functionViewGroup.pinAction = pin
+        functionViewGroup.quitAction = quit
+        functionViewGroup.boxAction = box
+        
+        self.view = functionViewGroup
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func preference() {
+        print("preference")
+    }
+    
+    func pin() {
+        print("pin")
+    }
+    
+    func quit() {
+        print("quit")
+        NSApplication.shared.terminate(self)
+    }
+    
+    func box() {
+        print("box")
+    }
+}
