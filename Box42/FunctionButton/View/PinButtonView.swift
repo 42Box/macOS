3babd97d86365ddb88adc358ab708c9bdd780078
chanhihi <chanhihi55@gmail.com>
2023-08-27 00:48:23 +0900

    fix: pin button 디자인 변경

--- Box42/FunctionButton/View/PinButtonView.swift
+++ Box42/FunctionButton/View/PinButtonView.swift
@@ -10,6 +10,7 @@ import AppKit
 class PinButtonView: NSButton {
     
     private var callback: (() -> Void)?
+    private var pinBoxLabel: NSTextField!
     
     init(image: NSImage, completion: @escaping () -> Void) {
         super.init(frame: .zero)
@@ -21,6 +22,21 @@ class PinButtonView: NSButton {
         self.target = self
         self.action = #selector(pin)
         self.callback = completion
+        
+        pinBoxLabel = NSTextField(labelWithString: "Pin Box")
+        pinBoxLabel.font = NSFont(name: "Inter", size: FunctionButtonUI.size.font)
+        pinBoxLabel.textColor = FunctionButtonUI.color.pinText
+        pinBoxLabel.backgroundColor = NSColor.clear
+        pinBoxLabel.isBordered = false
+        
+        self.addSubview(pinBoxLabel)  // 라벨을 버튼에 추가
+        
+        // 레이아웃을 설정 (예: AutoLayout)
+        pinBoxLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pinBoxLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            pinBoxLabel.leadingAnchor.constraint(equalTo: self.trailingAnchor, constant: 8)
+        ])
     }
     
     required init?(coder: NSCoder) {
