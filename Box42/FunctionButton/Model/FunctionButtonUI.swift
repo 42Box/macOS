f8824d683a7a3ae667fe20b9cfcaaec9421caadc
KIM CHAN HEE <85754295+chanhihi@users.noreply.github.com>
2023-08-27 00:49:07 +0900

    fix: pin button 디자인 변경 (#78)
    * fix: pin button 디자인 변경
    
    * fix: pin button font 및 label 추가

---
+++ Box42/FunctionButton/Model/FunctionButtonUI.swift
@@ -0,0 +1,21 @@
+//
+//  FunctionButtonUI.swift
+//  Box42
+//
+//  Created by Chanhee Kim on 8/27/23.
+//
+
+import AppKit
+
+struct FunctionButtonUI {
+    struct size {
+        static let font: CGFloat = 14
+    }
+    
+    struct animation {
+    }
+    
+    struct color {
+        static let pinText = NSColor(hex: "#696969")
+    }
+}
--- Box42/FunctionButton/View/PinButtonView.swift
+++ Box42/FunctionButton/View/PinButtonView.swift
@@ -10,17 +10,33 @@ import AppKit
 class PinButtonView: NSButton {
     
     private var callback: (() -> Void)?
+    private var pinBoxLabel: NSTextField!
     
     init(image: NSImage, completion: @escaping () -> Void) {
         super.init(frame: .zero)
         
         self.image = image
-        self.isBordered = false  // 버튼의 테두리를 제거
+        self.isBordered = false
         self.wantsLayer = true
         self.layer?.backgroundColor = NSColor.clear.cgColor
         self.target = self
         self.action = #selector(pin)
         self.callback = completion
+        
+        pinBoxLabel = NSTextField(labelWithString: "Pin Box")
+        pinBoxLabel.font = NSFont(name: "Inter", size: FunctionButtonUI.size.font)
+        pinBoxLabel.textColor = FunctionButtonUI.color.pinText
+        pinBoxLabel.backgroundColor = NSColor.clear
+        pinBoxLabel.isBordered = false
+        
+        self.addSubview(pinBoxLabel)  // 라벨을 버튼에 추가
+        
+        // 레이아웃을 설정 (예: AutoLayout)
+        pinBoxLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pinBoxLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            pinBoxLabel.leadingAnchor.constraint(equalTo: self.trailingAnchor, constant: 8)
+        ])
     }
     
     required init?(coder: NSCoder) {
