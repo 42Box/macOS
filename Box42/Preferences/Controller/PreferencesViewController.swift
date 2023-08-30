1cbdebf81c3de4872bd7f2b653de7102a6dbb954
chanhihi <chanhihi55@gmail.com>
2023-08-31 04:31:04 +0900

    refact: NSViewController을 규격에 맞게 수정합니다.

--- Box42/Preferences/Controller/PreferencesViewController.swift
+++ Box42/Preferences/Controller/PreferencesViewController.swift
@@ -9,29 +9,20 @@ import Cocoa
 import SnapKit
 
 class PreferencesViewController: NSViewController {
-    var prefTableView : NSTableView?
+    var prefTableView : PreferencesTableView?
     
     override func loadView() {
         self.view = NSView()
         self.view.wantsLayer = true
         self.view.layer?.backgroundColor = NSColor.blue.cgColor
         
-        prefTableView = NSTableView(frame: .zero)
-        // Column 추가
-        let column1 = NSTableColumn(identifier: NSUserInterfaceItemIdentifier("Column1"))
-        column1.width = 100.0
-        column1.title = "Column 1"
-        prefTableView?.addTableColumn(column1)
+        prefTableView = PreferencesTableView(frame: .zero)
+        prefTableView?.setup()
+//        prefTableView?.viewModel = viewModel
         
-        // delegate와 dataSource 설정
-        prefTableView?.delegate = self
-        prefTableView?.dataSource = self
-        
-        // TableView를 스크롤 뷰에 추가 (일반적으로 NSTableView는 NSScrollView 안에 위치합니다)
         let scrollView = NSScrollView()
         scrollView.documentView = prefTableView
         self.view.addSubview(scrollView)
-        
         
         scrollView.snp.makeConstraints({ make in
             make.edges.equalToSuperview()
@@ -42,20 +33,3 @@ class PreferencesViewController: NSViewController {
         })
     }
 }
-
-extension PreferencesViewController: NSTableViewDelegate, NSTableViewDataSource {
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return 10 // 총 로우 수
-    }
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier("MyCell"), owner: self) as? NSTableCellView ?? NSTableCellView()
-        cell.textField?.stringValue = "Row \(row), Column \(tableColumn?.identifier ?? NSUserInterfaceItemIdentifier(""))"
-        return cell
-    }
-
-    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        return 44.0 // 셀 높이를 44로 설정
-    }
-}
-
