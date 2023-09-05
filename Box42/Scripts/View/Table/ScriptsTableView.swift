cd0b8dc3cf48894af54785e8c2d40fedf66b4fd0
DaSol Kim <solda@khu.ac.kr>
2023-09-05 22:56:32 +0900

    feat: add pin button and automation setting button. (#138)
    * feat: add pin button and automation setting button.
    
    * chore: delete
    
    * chore: QuickSlotGroupView.swift
    
    ---------
    
    Co-authored-by: KIM CHAN HEE <85754295+chanhihi@users.noreply.github.com>

--- Box42/Scripts/View/Table/ScriptsTableView.swift
+++ Box42/Scripts/View/Table/ScriptsTableView.swift
@@ -32,6 +32,8 @@ class ScriptsTableView: NSTableView {
     func setup() {
         self.delegate = self
         self.dataSource = self
+        self.headerView = nil
+        self.backgroundColor = .white
         let column1 = NSTableColumn(identifier: NSUserInterfaceItemIdentifier("Scripts"))
         self.addTableColumn(column1)
     }
